1740d33d3a1cec74a83a372eafed3545d01e008c
Julia <jrwilkinson@quinnipiac.edu>
2021-11-04 15:23:30 -0400

    separate pages

---
+++ routes/addperson.js
@@ -0,0 +1,5 @@
+var addperson = {};
+addperson.add = function (counter) {
+    return counter + 1;
+};
+module.exports = addperson;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var multiply = require('./multiply');
+var math = require('./addperson');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Fitness Center Crowd Control Application' });
 });
 
-router.get('/api/multiply', function(req, res, next) {
+router.get('/api/multiply', function (req, res, next) {
   var n1 = parseInt(req.query.n1);
   var n2 = parseInt(req.query.n2);
-  res.json(multiply.add(n1,n2));
+  res.json(multiply.add(n1, n2));
 });
 
+// Set up additional routes here
+router.get('/maincampus', function (req, res, next) {
+  res.render('maincampus', { title: 'Fitness Center Crowd at Mount Carmel' });
+});
+router.get('/yorkhill', function (req, res, next) {
+  res.render('yorkhill', { title: 'Fitness Center Crowd at York Hill' });
+
+
+
+  //res.send(counter);
+  //var counter = parseInt(req.query.counter);
+  //res.json(math.add(counter, 1));
+});
+
+//router.get('/api/addperson', function (req, res, next) {
+// var counter = parseInt(req.query.counter);
+//logger.info('/api/add Used to add ' + counter + 'and 1 person')
+// res.json(math.add(counter, 1));
+//});
 
 module.exports = router;
